62bb703b09cce8de31f5c07188f1e39c09ce6f6b
MichelleTu <1139929966@qq.com>
2017-03-28 17:52:50 +0800

    Update code.R

--- code.R
+++ code.R
@@ -1,51 +1,73 @@
+library(rpart)
+library(C50)
+library(ROSE)
+library(pROC)
+library(caret)
+
 SamplingSolution <- 
-  function(form, data, ways, sampling = NULL,...)
+  function(form, Churn.data_s, ways, sampling = NULL,...)
   {
-    library(caret)
     tgt <- which(names(data) == as.character(form[[2]]))
-    EX  <- vector("list", 2)
+    Ex <- vector("list", 2)
     
-    for (i in 1:2)
+    for(i in 1:2)
     {
-      #data division
-      id <- createDataPartition(data[,tgt], p = 1/2, list = FALSE)
-      fold1 <- data[id, ]
-      fold2 <- data[-id, ]
-      fold1New <- fold1
-      fold2New <- fold2
       
-      if (!is.null(sampling))
+      # data division
+      id <- createDataPartition(Churn.data_s$churn, p = 1/2,list = FALSE)
+      fold1 <- Churn.data_s[id,]
+      fold2 <- Churn.data_s[-id,]
+      fold1_new <- fold1
+      fold2_new <- fold2
+      
+      
+      if(!is.null(sampling))
       {
-        sourcefile <- paste(sampling, c(".R"), sep="")
+        sourcefile <- paste(sampling, c("SMOTE.R"), sep = "")
         source(sourcefile)
-        fold1New <- do.call(sampling, list(form, fold1, ...))
-        fold2New <- do.call(sampling, list(form, fold2, ...))
-      }  
+        fold1_new <- do.call(sampling, list(form, fold1,...))
+        fold2_new <- do.call(sampling, list(form, fold2,...))
+      }
       
-      ModelFold1 <- ways$fit(form, fold1New)
-      trainingScoreFold1 <- ways$pred(ModelFold1, fold1)
-      testingScoreFold1  <- ways$pred(ModelFold1, fold2)
+
+      Model_fold1 <- ways$fit(form, fold1_new)
+      training_Score_fold1 <- ways$pred(Model_fold1, fold1)
+      testing_score_fold1  <- ways$pred(Model_fold1, fold2)
       
-      ModelFold2 <- ways$fit(form, fold2New)
-      trainingScoreFold2 <- ways$pred(ModelFold2, fold2)
-      testingScoreFold2  <- ways$pred(ModelFold2, fold1)
-      EX[[i]] <- 
-        list(fold1Label = fold1[, tgt],
-             fold2Label = fold2[, tgt],
-             fold1TrainingScore = trainingScoreFold1,
-             fold2TrainingScore = trainingScoreFold2,
-             fold1TestingScore = testingScoreFold1,
-             fold2TestingScore = testingScoreFold2      
-        )
+      Model_fold2 <- ways$fit(form,fold2_new)
+      training_score_fold2 <- ways$pred(Model_fold2, fold2)
+      testing_score_fold2  <- ways$pred(Model_fold2,fold1)
+      
+      Ex[[i]] <- 
+        list(fold1_label = fold1[,tgt],
+             fold2_label = fold2[,tgt],
+             fold1_training_score = training_Score_fold1,
+             fold2_training_score = training_score_fold2,
+             fold1_testing_score = testing_score_fold1,
+             fold2_testing_score = testing_score_fold2
+             )
     }
     return(EX)
   }
 
+
 # CART
 cart_tree <- list(
-  fit = function (form, data) { 
-    library(rpart)
-    model<- rpart(form, data, method = "class")
+  fit = function(form,data){
+    model <- rpart(form, data, method = "class")
+    return(model)
+  },
+  pred = function(object, data){
+    out <- predict(object,newdata = data, type = "prob")
+    out <- out[ ,2]
+  }
+)
+
+
+# C50
+C50_tree <-list(
+  fit = function(form, data){
+    model <- C5.0(form, data)
     return(model)
   },
   pred = function(object, data){
@@ -54,15 +76,3 @@ cart_tree <- list(
   }
 )
 
-# C50
-C50_tree <- list(
-  fit = function(form,data){
-    library(C50)
-    model <- C5.0(form, data)
-    return(model)
-  },
-  pred = function(object,data){
-    out <- predict(object, newdata = data, type = "prob")
-    out <- out[,2]
-  }
-)
